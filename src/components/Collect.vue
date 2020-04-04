ce15225da836f993b6e1b58bf4c3450aa2a23741
zhangzhiling <435755479@qq.com>
2020-04-05 06:35:05 +0800

    checkall实现

--- src/components/Collect.vue
+++ src/components/Collect.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <el-select v-model="value" placeholder="请选择">
+    <!-- <el-select v-model="value" placeholder="请选择">
       <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
     </el-select>
     <el-button type="success" round @click="submit('search')">成功按钮</el-button>
@@ -14,17 +14,17 @@
       <el-table-column prop="name" label="姓名" width="180"></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
     </el-table>
-    <!-- <el-pagination
-      background
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total"
-      :page-sizes="[10, 20, 30, 40, 50, 100]"
-      :page-size="pageSize"
-      @size-change="handleSizeChange"
-      @current-change="current"
-      :current-page.sync="currentPage"
-    ></el-pagination>-->
-    <Page :total="total" :pageSizes="[10,15,20]" :pageSize="pageSize" @handleSizeChange="handleSizeChange" @current="current" :cur="currentPage"></Page>
+    <Page :total="total" :pageSizes="[10,15,20]" :pageSize="pageSize" @handleSizeChange="handleSizeChange" @current="current" :cur="currentPage"></Page>-->
+    <el-checkbox v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+    <div style="margin: 15px 0;"></div>
+    <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
+      <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+    </el-checkbox-group>
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    checkall{{checkAll}}
   </div>
 </template>
 <script>
@@ -32,62 +32,93 @@ import * as TYPES from "../store/mutations-types.js";
 import { Table, Button } from "element-ui";
 import { getTable } from "../api/index";
 import Page from "../components/page";
+
+// const cityOptions = ["上海", "北京", "广州", "深圳"];
 export default {
   data() {
     return {
-      msg: "",
-      tableData: [],
-      options: [
-        {
-          value: 1,
-          label: "王小虎"
-        },
-        {
-          value: 2,
-          label: "王小"
-        }
-      ],
-      value: "",
-      total: 10,
-      currentPage: 1,
-      pageSize: 10
+      checkAll: true,
+      checkedCities: [],
+      cities: ["上海", "北京", "广州", "深圳"],
+      isIndeterminate: true
     };
   },
-  async created() {
-    let data = await getTable({
-      name: ""
-    });
-    // console.log(data);
-    this.tableData = data.data;
-    this.total = data.total;
+  created() {
+    for (let i = 0; i < this.cities.length; i++) {
+      let item = this.cities[i];
+      this.checkedCities.push(item);
+    }
   },
   methods: {
-    async submit(val) {
-      let res = await getTable({
-        name: this.value
-      });
-      let { data } = res;
-      this.tableData = data;
-      this.total = res.total;
-      this.currentPage = res.cur;
+    handleCheckAllChange(val) {
+      console.log(val);
+      this.checkedCities = val ?this.cities : [];
+      this.isIndeterminate = false;
     },
-    handleSizeChange(val) {
-      this.pageSize = val;
-    },
-    current(val) {
-      this.currentPage = val;
+    handleCheckedCitiesChange(value) {
+      let checkedCount = value.length;
+      this.checkAll = checkedCount === this.cities.length;
+      this.isIndeterminate =
+        checkedCount > 0 && checkedCount < this.cities.length;
     }
-  },
-  computed: {
-    getValue() {
-      return this.$store.state.prod;
-    },
-    getVal() {
-      return this.$store.getters.getValue;
-    }
-  },
-  components: { Table, Button, Page }
+  }
 };
+// export default {
+//   data() {
+//     return {
+//       msg: "",
+//       tableData: [],
+//       options: [
+//         {
+//           value: 1,
+//           label: "王小虎"
+//         },
+//         {
+//           value: 2,
+//           label: "王小"
+//         }
+//       ],
+//       value: "",
+//       total: 10,
+//       currentPage: 1,
+//       pageSize: 10
+//     };
+//   },
+//   async created() {
+//     let data = await getTable({
+//       name: ""
+//     });
+//     // console.log(data);
+//     this.tableData = data.data;
+//     this.total = data.total;
+//   },
+//   methods: {
+//     async submit(val) {
+//       let res = await getTable({
+//         name: this.value
+//       });
+//       let { data } = res;
+//       this.tableData = data;
+//       this.total = res.total;
+//       this.currentPage = res.cur;
+//     },
+//     handleSizeChange(val) {
+//       this.pageSize = val;
+//     },
+//     current(val) {
+//       this.currentPage = val;
+//     }
+//   },
+//   computed: {
+//     getValue() {
+//       return this.$store.state.prod;
+//     },
+//     getVal() {
+//       return this.$store.getters.getValue;
+//     }
+//   },
+//   components: { Table, Button, Page }
+// };
 </script>
 <style scoped>
 </style>
